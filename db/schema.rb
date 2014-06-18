52dc9685a956ba284196ab50df82f325d34a332b
Ken Luk <knnyluk@gmail.com>
2014-06-17 20:58:15 -0400

    added route/stop associations and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140618002750) do
+ActiveRecord::Schema.define(version: 20140618005126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "routes", force: true do |t|
     t.string "number"
+  end
+
+  create_table "routes_stops", id: false, force: true do |t|
+    t.integer "route_id", null: false
+    t.integer "stop_id",  null: false
   end
 
   create_table "stops", force: true do |t|
--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -2,4 +2,5 @@ require 'spec_helper'
 
 describe Route do
   it { should have_db_column(:number) }  
+  it { should have_many(:stops) }
 end
